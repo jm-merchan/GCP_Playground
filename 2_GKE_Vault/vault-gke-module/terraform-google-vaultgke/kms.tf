fe79b7858f8fc7f01e0a55199454df654a0e822e
jm-merchan <jose.merchan@hashicorp.com>
2024-10-03 18:51:07 +0200

    Enabling KMS API

--- 2_GKE_Vault/vault-gke-module/terraform-google-vaultgke/kms.tf
+++ 2_GKE_Vault/vault-gke-module/terraform-google-vaultgke/kms.tf
@@ -8,3 +8,21 @@ resource "google_kms_crypto_key" "vault_key" {
   key_ring = google_kms_key_ring.key_ring.id
   purpose  = "ENCRYPT_DECRYPT"
 }
+
+# Enable Cloud KMS API - https://console.developers.google.com/apis/api/cloudkms.googleapis.com/overview?
+resource "google_project_service" "cloudkms" {
+  project = var.project_id
+  service = "cloudkms.googleapis.com"
+
+  timeouts {
+    create = "30m"
+    update = "40m"
+  }
+
+  disable_on_destroy = false
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  depends_on      = [google_project_service.cloudkms]
+  create_duration = "60s"
+}
